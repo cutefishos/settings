4ce06e29a95bf1e3a889e221f15d2174a78dda82
reionwong <reionwong@gmail.com>
2026-09-02 12:24:50 -0400

    refactor(settings): drop no-op Qt5 high DPI attributes

--- src/main.cpp
+++ src/main.cpp
@@ -23,9 +23,6 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling, true);
-
     Application app(argc, argv);
 
     app.setWindowIcon(QIcon::fromTheme("cutefish-settings"));
